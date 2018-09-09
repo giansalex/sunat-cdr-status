9343b912e1927c230e815a76d3e644449b0e9133
Giancarlos Salas <giansalex@gmail.com>
2018-09-09 12:36:38 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace CdrFacturaPrueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls;
+            using (var service = new ServiceReference1.billServiceClient())
+            {
+                var result = service.getStatus("201234567891", "01", "F001", 200);
+                Console.WriteLine("StatuCode :" + result.statusCode + " -> " + result.statusMessage);
+                Console.ReadKey();
+            }
+        }
+    }
+}
